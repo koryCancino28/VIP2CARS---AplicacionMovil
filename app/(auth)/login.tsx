05ca97d076cf1e8fa0ef59e323ad02dec80e8832
SistemaPegaso <jerryprograminpy@gmail.com>
2025-09-09 14:50:33 -0500

    feat: :sparkles: Main arreglado, dime gracias papi, soy tu salvador, dime gracias

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -7,14 +7,12 @@
 
 import CustomText from "@/components/CustomText";
 import { Link, router } from "expo-router";
-<<<<<<< HEAD
+
 import React, { useState } from "react";
 import { Image, Switch, TextInput, TouchableOpacity, View } from "react-native";
 
 const icon = require('../../assets/images/logo-vip2cars.png');
-=======
-import { Image, Switch, Text, TextInput, TouchableOpacity, View } from "react-native";
->>>>>>> Henry
+
 
 export default function LoginScreen() {
   const [dni, setDni] = useState('');
@@ -32,7 +30,6 @@ export default function LoginScreen() {
 
   return (
     <View style={{ flex: 1, backgroundColor: "#000", padding: 24, justifyContent: "center" }}>
-<<<<<<< HEAD
       {/*Logo*/}
       <Image source={icon} style={{ width: 380, height: 60, alignSelf: "center", marginBottom: 20 }}></Image>
       {/*Formulario*/}
@@ -79,61 +76,13 @@ export default function LoginScreen() {
 
           {/* Botón Ingresar */}
           <View style={{ width: "100%", maxWidth: 200 }}>
-            <TouchableOpacity style={{ backgroundColor: "#E4022E", borderRadius: 22, paddingVertical: 15, paddingHorizontal: 24, alignItems: 'center' }} onPress={onSubmit}>
+            <TouchableOpacity style={{ backgroundColor: "#E4022E", borderRadius: 22, paddingVertical: 15, paddingHorizontal: 24, alignItems: 'center' }} onPress={handleLogin}>
               <CustomText style={{ color: 'white', fontSize: 18 }}>INICIAR SESIÓN</CustomText>
             </TouchableOpacity>
           </View>
 
         </View>
       </View>
-
-=======
-      <Image
-        source={require("../../assets/images/vip2cars_logo.png")} 
-        style={{ width: 350, height: 120, alignSelf: "center", marginBottom: 10, marginTop: -80 }}
-        resizeMode="contain"
-      />
-      <View
-        style={{
-          backgroundColor: "#fff",
-          borderRadius: 16,
-          padding: 24,
-        }}
-      >
-      <Text style={{ color: "#000000ff", fontSize: 20, fontWeight: "600", alignSelf:"center", marginBottom: 15 }}>INICIO DE SESIÓN</Text>
-
-      {/* DNI */}
-      <Text style={{ color: "#080808ff", marginBottom: 5 }}>DNI</Text>
-      <TextInput style={{ backgroundColor: "#ecececff", color: "#000000ff", borderRadius: 8, padding: 12, marginBottom: 12}} />
-
-      {/* Contraseña */}
-      <Text style={{ color: "#030303ff", marginBottom: 5 }}>CONTRASEÑA</Text>
-      <TextInput secureTextEntry style={{ backgroundColor: "#ecececff", color: "#000000ff", borderRadius: 8, padding: 12, marginBottom: 12}} />
-
-      {/* Recordarme */}
-      <View style={{ flexDirection: "row", alignItems: "center", marginBottom: 10 }}>
-        <Switch />
-        <Text style={{ color: "#000000ff", marginLeft: 8 }}>Recordarme en este dispositivo</Text>
-      </View>
-
-      {/* Botón personalizado */}
-        <TouchableOpacity
-          onPress={handleLogin}
-          style={{
-            backgroundColor: "#E1052A", 
-            paddingVertical: 14,
-            borderRadius: 8,
-            alignItems: "center",
-          }}
-        >
-          <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 16 }}>Ingresar</Text>
-        </TouchableOpacity>
-      <View style={{ height: 12 }} />
-      <Link href="../(auth)/password/step1-dni" asChild>
-        <Text style={{ color: "#E1052A", alignSelf: "center" }}>¿Olvidaste tu contraseña?</Text>
-      </Link>
-      </View>
->>>>>>> Henry
     </View>
   );
 }
--- app/(auth)/password/step2-code.tsx
+++ app/(auth)/password/step2-code.tsx
@@ -6,14 +6,12 @@
 
 import CustomText from "@/components/CustomText";
 import { router, useLocalSearchParams } from "expo-router";
-<<<<<<< HEAD
+
 import { useState } from "react";
 import { Image, TextInput, TouchableOpacity, View } from "react-native";
 
 const icon = require('../../../assets/images/logo-vip2cars.png');
-=======
-import { Image, Pressable, Text, TextInput, TouchableOpacity, View } from "react-native";
->>>>>>> Henry
+
 
 export default function Step2Code() {
   const { dni } = useLocalSearchParams<{ dni: string }>();
@@ -24,7 +22,6 @@ export default function Step2Code() {
   };
   return (
     <View style={{ flex: 1, backgroundColor: "#000", justifyContent: "center", padding: 24 }}>
-<<<<<<< HEAD
       {/*Logo*/}
       <Image source={icon} style={{ width: 380, height: 60, alignSelf: "center", marginBottom: 20 }}></Image>
       {/*Formulario*/}
@@ -73,38 +70,6 @@ export default function Step2Code() {
             </CustomText>
           </TouchableOpacity>
         </View>
-=======
-      <Image
-        source={require("../../../assets/images/vip2cars_logo.png")} 
-        style={{ width: 350, height: 120, alignSelf: "center", marginBottom: 10, marginTop: -90 }}
-        resizeMode="contain"
-      />
-      <View
-        style={{
-          backgroundColor: "#fff",
-          borderRadius: 16,
-          padding: 24,
-        }}
-      >
-      <Text style={{ color: "#0f0f0fff", fontWeight: "700", fontSize: 18, marginBottom: 10, alignSelf: "center" }}>RECUPERAR CONTRASEÑA</Text>
-      <Text style={{ color: "#080808ff", marginBottom:10 }}>Ingrese el código enviado al número asociado al DNI {dni}</Text>
-      <TextInput style={{ backgroundColor: "#ecececff", color: "#000000ff", borderRadius: 10, padding: 12, marginBottom: 16 }} />
-      <Pressable onPress={() => {/* Api.resendCode(dni) */}}>
-        <Text style={{ color: "#E1052A", marginBottom: 12 }}>Reenviar código</Text>
-      </Pressable>
-      {/* Botón personalizado */}
-      <TouchableOpacity
-        onPress={onContinue}
-        style={{
-          backgroundColor: "#E1052A", 
-          paddingVertical: 14,
-          borderRadius: 8,
-          alignItems: "center",
-        }}
-      >
-        <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 16 }}>Continuar</Text>
-      </TouchableOpacity>
->>>>>>> Henry
       </View>
     </View>
   );
