a8fd8622833e514889b3bda5ae6852efd0978452
Kory Cancino <koryna654@gmail.com>
2025-09-09 14:03:06 -0500

    prueba

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,32 @@
-import 'react-native-reanimated';
+// app/_layout.tsx
+import { Tabs } from 'expo-router';
 
-
-export default function RootLayout() {
-  
-  return null;
+export default function Layout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false, // Oculta el header superior (opcional)
+        tabBarActiveTintColor: 'blue', // Color del tab activo
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Inicio',
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Perfil',
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Ajustes',
+        }}
+      />
+    </Tabs>
+  );
 }
